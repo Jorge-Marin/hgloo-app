1e90737215487dee6ee13e5a89e03d8fd791c064
Jorge-Marin <jorgemarin9915@gmail.com>
2021-03-12 18:22:20 -0600

    Giving functionality to login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,14 +11,40 @@ import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  user: any;
+  localStorage = window.localStorage;
   loginForm: FormGroup = this.formBuilder.group({
-    name: ['', Validators.required ],
+    email: ['', Validators.required ],
     password: ['', Validators.required],
   });
 
   constructor( private formBuilder: FormBuilder,
-    private auth: AuthService ) { }
+    private auth: AuthService,
+    private router: Router ) {
+     }
 
-  ngOnInit(): void {}
-    
+  ngOnInit(): void {
+    this.auth.isAuthenticated().then( user => {
+      if ( localStorage.getItem('token') ) {
+        if ( user ) {
+          user.getIdToken().then( token => {
+            const tokenData = JSON.parse( localStorage.getItem('token') );
+            console.log(tokenData[0].token);
+            if ( tokenData[0].token === token) {
+              this.router.navigate(['/dashboard']);
+            }
+          });
+        }
+      }
+    });
   }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+  login() {
+   if ( this.loginForm.valid ) {
+      this.auth.login( this.email.value, this.password.value)
+   }
+  }
+}
